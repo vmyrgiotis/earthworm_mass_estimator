b1001df9d31639bc6b6f479e7c7e8fdf1eb4723f
Vasilis Myrgiotis <38428015+vmyrgiotis@users.noreply.github.com>
2026-04-28 23:23:26 +0100

    Improve instructions for app functionality

--- streamlit_photo_app.py
+++ streamlit_photo_app.py
@@ -295,7 +295,7 @@ st.write(
     " Try to centre the photo close to the centre of the A4 sheet." 
     " Use good lighting for best results."
     " Turn the camera flash on to avoid strong shadows and reflections on the paper - if needed.  \n\n "
-    " The app detects the A4 outline and object mask, and estimates area covered by earthworms in cm². "    
+    " The app 1. detects the A4 outline 2. masks all earthworms on the A4 and 4. estimates area covered by earthworms in cm². "    
     " Adjust detection parameters in the sidebar if needed."
     " The Minimum object size (pixels) parameter can be used to exclude small debris or other noise."
 )
