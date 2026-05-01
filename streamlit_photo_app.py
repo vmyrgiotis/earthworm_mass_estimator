029a17020461526aa5fa5ee39112c74a5bb404fb
Vasilis Myrgiotis <38428015+vmyrgiotis@users.noreply.github.com>
2026-05-01 10:11:43 +0100

    Update streamlit_photo_app.py

--- streamlit_photo_app.py
+++ streamlit_photo_app.py
@@ -1,17 +1,3 @@
-import sys
-import streamlit as st
-
-st.write(sys.version)
-st.write(sys.executable)
-
-try:
-    import cv2
-    st.write(cv2.__version__)
-except Exception as e:
-    st.error(repr(e))
-    st.stop()
-    
-    import cv2
 import numpy as np
 import streamlit as st
 from PIL import Image
